dd31aec294298202e788e1cea131ca472ee602e4
McKeever <alex.mckeever@tsi.corp.com>
2019-09-10 17:52:35 -0500

    Cleanup and add favicon

--- flask_server/f_server.py
+++ flask_server/f_server.py
@@ -6,17 +6,17 @@ import subprocess
 app = Flask(__name__)
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 @app.route('/turnon/', methods=['POST'])
 def move_forward():
     forward_message = "Moving forward..."
     subprocess.Popen("./send_command.sh")
     print("THIS IS A TEST")
-    return render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/turnoff/', methods=['POST'])
 def move_backward():
     subprocess.Popen("./send_stop.sh")
     print("THIS IS A STOP")
-    return render_template('base.html')
+    return render_template('index.html')
